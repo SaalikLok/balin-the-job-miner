69627204692fe1498a815648d42591f6dea9d0d1
Saalik Lokhandwala <saaliklok@Saaliks-MBP.cable.rcn.com>
2021-05-18 15:50:10 -0400

    added jobs class

---
+++ lib/job.rb
@@ -0,0 +1,10 @@
+class ClassName
+  def initialize(title, link, score, location, company, date_posted)
+    @title = title
+    @link = link
+    @score = score
+    @location = location
+    @company = company
+    @date_posted = date_posted
+  end
+end
