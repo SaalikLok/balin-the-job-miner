30cabb41ca1fe666ba329a69682a10a92906956d
Saalik Lokhandwala <saaliklok@Saaliks-MBP.cable.rcn.com>
2021-05-25 07:02:16 -0400

    added quotes from lord of the rings to index

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 
 gem "sinatra"
-gem 'sinatra-contrib'
+gem "sinatra-contrib"
+gem "dotenv"
+gem "faraday"
 
 gem "nokogiri"
 gem "haml"
--- server.rb
+++ server.rb
@@ -1,13 +1,19 @@
+require 'dotenv/load'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'haml'
 require_relative './lib/scraper'
+require_relative './lib/lotr_quote'
 
 set :haml, :format => :html5
 
 get '/' do
   scraper = Scraper.new
   @jobs = scraper.get_jobs
+
+  quote = Lotr_quote.new
+  @quote_text = quote.quote_text
+  @quote_author_name_race = quote.author_name_race
 
   haml :index
 end
